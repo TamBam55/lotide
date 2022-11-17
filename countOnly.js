2cb60f1ba5214951cd1de81f12cbd28c963a72ce
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-11-16 21:58:27 -0800

    Unable to execute correct output, but code doing half job

---
+++ countOnly.js
@@ -0,0 +1,53 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.assert(true, actual !== expected);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.assert(true, actual === expected);
+  }
+};
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const item of allItems) {
+    console.log(item);
+  }
+
+  // inside the loop, 
+// increment the counter for each item:
+//   set a property with that string key to:
+//     the value that was already there (or zero if nothing there) with 1 added to it.
+// inside the loop:
+if (itemsToCount[item]) { 
+  if (results[item]) {
+    results[item] += 1;
+  } else {
+    results[item] = 1;
+  }
+}
+  return results;
+}
+/* not sure how this is supppose to run. There is going to be undefined variables, but it will show the names.? */
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
