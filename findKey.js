dbe0c250beb36162f8112ce599ee96f483707911
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-11-18 00:00:01 -0800

    Not grasping

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  let objKeys = Object.keys(obj);
+  let index = 0;
+  for (let element in obj) {
+    if (obj[element] === val) {
+      return objKeys[index];
+    }
+    index++;
+  }
+  return undefined;
+}
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKey = function(obj, clbk) {
+  if (var i =0; )
+
+}
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
