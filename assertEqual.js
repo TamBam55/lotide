27187e9b9f19c6421679358e636d9038b80e7b52
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-11-20 00:24:13 -0800

    Adding updates and notes

--- assertEqual.js
+++ assertEqual.js
@@ -1,44 +1,14 @@
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
-
-
-// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`👎👎👎 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-const findKeyByValue = function(obj, val) {
-  let objKeys = Object.keys(obj);
-  let index = 0;
-  for (let element in obj) {
-    if (obj[element] === val) {
-      return objKeys[index];
-    }
-    index++;
-  }
-  return undefined;
-}
-
-
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-//Consider using Object.keys func to help search through all the object keys.
-
-//Consider using for...of to loop over the keys returned by Object.keys.
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(55, 55);
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual("Tammy", "TaMmy");
