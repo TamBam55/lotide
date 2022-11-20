0e15c3aef510c8950e9f8939867eaba0d904b270
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-11-19 21:46:44 -0800

    applying all changes

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,18 @@
+const eqObjects = function(object1, object2){
+  if (Object.keys(object1).length === Object.keys(object2).length){
+    for(let key of Object.keys(object1) ){
+        if(Array.isArray(object1[key]) && Array.isArray(object2[key])){
+            if (!eqArrays(object1[key], object2[key])){
+             return false;       
+            }
+        }
+        
+    }
+    return true;
+  }
+return false;    
+};
+
 const eqArrays = function(firstArray, secondArray) {
   for (let i = 0; i < firstArray.length; i++) {
     if (firstArray[i] !== secondArray[i]) {
@@ -9,12 +24,14 @@ const eqArrays = function(firstArray, secondArray) {
 
 // FUNCTION IMPLEMENTATION
 const assertObjectsEqual = function(actual, expected) {
-  // Implement me!
-  const inspect = require('util').inspect;
-  if (eqObjects(actual), expectedObject)) {
-    console.log(`✅ Assertation Passed: ${inspect(actual))} === ${inspect(expectedObject)}`);
+  const inspect = require(`util`).inspect;
+  if (actual !== expected) {
+   console.log(`🛑🛑🛑Assertion Failed:  ${inspect(ab)} !== ${inspect(ba)}`);   
   } else {
-    console.log(`🔴 Assertation Failed: ${inspect(actual))} !== ${inspect(expectedObject)}`);
+   console.log(`✅✅✅Assertion Passed:  ${inspect(ab)} === ${inspect(ba)}`); 
   }
+}
 
-};
+  const ab = { a: "4", b: "2"};
+  const ba = { b: "2", a: "1"};
+  console.log(assertObjectsEqual(eqObjects(ab,ba),false));
--- eqObjects.js
+++ eqObjects.js
@@ -6,6 +6,15 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const eqArrays = function(firstArray, secondArray) {
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 
 const eqObjects = function(object1, object2){
   if (Object.keys(object1).length === Object.keys(object2).length){
@@ -23,3 +32,18 @@ return false;
 };
 
 
+// Test Code - Primitive Values
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(assertEqual(eqObjects(ab, ba), true));
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(assertEqual(eqObjects(ab, abc), false));
+
+// Test Code - Array Values
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(assertEqual(eqObjects(cd, dc), true));
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(assertEqual(eqObjects(cd, cd2), false));
