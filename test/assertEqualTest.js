7310f077474f4e7a896052408008eabeac065102
Tammy-Lee <tammy-lee.plamondon@telus.com>
2022-11-25 00:42:13 -0800

    Test code file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(55, 55);
+assertEqual("Lighthouse", "Lighthouse");
+assertEqual("Tammy", "TaMmy");
